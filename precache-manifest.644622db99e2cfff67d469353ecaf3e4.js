c4b4856ac6ed65efe2ceced78753b3a3e5d05ab7
Shrijith <53373519+shrijith007@users.noreply.github.com>
2021-04-07 15:41:31 +0530

    Updates

---
+++ precache-manifest.644622db99e2cfff67d469353ecaf3e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592c99e993df33e825f271ab4ffd57b5",
+    "url": "/joblisting/index.html"
+  },
+  {
+    "revision": "78b7680f50da21e031f1",
+    "url": "/joblisting/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5e6347d51224c72d8272",
+    "url": "/joblisting/static/js/2.64e13c58.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/joblisting/static/js/2.64e13c58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b7680f50da21e031f1",
+    "url": "/joblisting/static/js/main.617838b3.chunk.js"
+  },
+  {
+    "revision": "78fc3d446785dd61b2cf",
+    "url": "/joblisting/static/js/runtime-main.7a8700e8.js"
+  }
+]);
